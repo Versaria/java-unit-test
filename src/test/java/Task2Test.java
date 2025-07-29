c97972fc5378e68161cd7d680df8630bb61d1b8e
Versaria <steep.evropa@mail.ru>
2025-07-30 01:37:06 +0300

    Fix test classes structure

---
+++ src/test/java/Task2Test.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ProgramTest {
+    static class Program {
+        public boolean checkIsAdult(int age) {
+            return age >= 18;
+        }
+    }
+  
+   @Test
+   public void checkIsAdultWhenAgeIsMoreThan18True() {
+       Program program = new Program();
+       boolean isAdult = program.checkIsAdult(19);
+       assertEquals("Должно вернуться true, так как пользователь совершеннолетний",
+                   true, isAdult);
+   }
+
+   @Test
+   public void checkIsAdultWhenAgeIsLessThan18False() {
+       Program program = new Program();
+       boolean isAdult = program.checkIsAdult(17);
+       assertEquals("Должно вернуться false, так как пользователь несовершеннолетний",
+                   false, isAdult);
+   }
+
+   @Test
+   public void checkIsAdultWhenAgeIs18True() {
+       Program program = new Program();
+       boolean isAdult = program.checkIsAdult(18);
+       assertEquals("Должно вернуться true, так как пользователю ровно 18 лет",
+                   true, isAdult);
+   }
+}
